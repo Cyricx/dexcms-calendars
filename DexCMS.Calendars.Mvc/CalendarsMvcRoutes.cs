1ffb95cf0fc6acd182aad8f6dc02841597439ab5
Cyricx <contact@chrisbyram.com>
2016-11-25 18:16:06 -0600

    Added default routes to Calendars MVC

---
+++ DexCMS.Calendars.Mvc/CalendarsMvcRoutes.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Routing;
+using System.Web.Mvc;
+using DexCMS.Core.Infrastructure.Models;
+
+namespace DexCMS.Calendars.Mvc
+{
+    public static class CalendarsMvcRoutes
+    {
+        public static void CreateDefaultRoutes(RouteCollection routes, DexCMSConfiguration config)
+        {
+            routes.MapRoute(
+                name: "Calendar",
+                url: "Calendar",
+                defaults: new { category = "none", urlSegment = "calendar", controller = "Calendar", action = "Index" });
+
+            routes.MapRoute(
+                name: "CalendarSubMethods",
+                url: "Calendar/{action}",
+                defaults: new { controller = "Calendar" });
+        }
+    }
+}
